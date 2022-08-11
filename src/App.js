e4d2ec4162d41ca372ba0c531eb5f7c2a34b5cc6
shindeAkshu <sakshata416@gmail.com>
2022-08-11 10:11:23 -0700

    Shopping-List

--- src/App.js
+++ src/App.js
@@ -13,15 +13,18 @@ function App() {
     <div className="App-background">
       <div className='main-container'>
         <div className='add-item-box' >
-          <input className='add-item-input' placeholder='Add an Item...'>
-
-          </input>
-
-        </div>
+          <input className='add-item-input' placeholder='Add an Item...'/>
+            <fontawesomeIcon icon={FaPlus}/>
+          </div>
+          <div className='item-list'>
+            <div className='item-container'>
+              <div className='item-name'>-</div>
+              <div className='quantity'>-</div>
+            </div> 
+          </div>
+          <div className='total'> Total: 6</div>
       </div>
-
-    
-    </div>
+   </div>
   );
 }
 
