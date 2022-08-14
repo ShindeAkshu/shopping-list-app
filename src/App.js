70ea50c90385688ffeea5baa194a1be322366757
shindeAkshu <sakshata416@gmail.com>
2022-08-14 09:22:50 -0700

    Shopping-List

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState,useEffect } from 'react';
 import './App.css';
 import './index.css';
+import {fontawesomeIcon} from '@fontawesome/react-fontawesomeIcon';
 
 function App() {
   const [items,setItems] =useState([
